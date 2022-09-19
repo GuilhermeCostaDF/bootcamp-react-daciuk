7f652530150486c925f7389db065c6db185cee42
GuidoCostaDF <guidocostadf@gmail.com>
2022-09-19 13:08:07 -0300

    iniciando desafio 1

--- desafio01/src/app.js
+++ desafio01/src/app.js
@@ -1,7 +1,15 @@
+import Fragment from "react";
+
 function App() {
   return (
     <>
-      <h3>Hello World!</h3>
+      <header>Header</header>
+      <nav>Menu</nav>
+      <main>
+        <aside>Sidebar</aside>
+        <section>Conteudo</section>
+      </main>
+      <footer>Footer</footer>
     </>
   );
 }
--- desafio01/src/index.js
+++ desafio01/src/index.js
@@ -1,4 +1,5 @@
 import App from "./app";
 import ReactDOM from "react-dom";
+import { style } from "./style.css";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
